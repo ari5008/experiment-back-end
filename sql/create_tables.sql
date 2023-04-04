71405119d59edf7a707e238ce5d5b7e7b3df0480
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-04-04 12:12:55 +0900

    refactor: #4 create_table.sqlの記述変更

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,64 +2,32 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.5 (Debian 14.5-1.pgdg110+1)
--- Dumped by pg_dump version 14.5 (Homebrew)
+-- Dumped from database version 14.7 (Debian 14.7-1.pgdg110+1)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
 -- Name: genres; Type: TABLE; Schema: public; Owner: -
---
+--  
 
 CREATE TABLE public.genres (
-    id integer NOT NULL,
-    genre character varying(255),
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    id          serial primary key,
+    genre       character varying(255),
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone
 );
-
-
---
--- Name: genres_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-ALTER TABLE public.genres ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.genres_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
 
 --
 -- Name: movies; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.movies (
-    id integer NOT NULL,
-    title character varying(512),
-    release_date date,
-    runtime integer,
-    mpaa_rating character varying(10),
-    description text,
+    id              serial primary key,
+    title           character varying(512),
+    release_date    date,
+    runtime         integer,
+    mpaa_rating     character varying(10),
+    description     text,
     image character varying(255),
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    created_at      timestamp with time zone,
+    updated_at      timestamp with time zone
 );
 
 
@@ -68,66 +36,23 @@ CREATE TABLE public.movies (
 --
 
 CREATE TABLE public.movies_genres (
-    id integer NOT NULL,
-    movie_id integer,
-    genre_id integer
+    id          serial primary key,
+    movie_id    integer references public.movies(id),
+    genre_id    integer references public.genres(id)
 );
-
-
---
--- Name: movies_genres_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-ALTER TABLE public.movies_genres ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.movies_genres_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
--- Name: movies_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-ALTER TABLE public.movies ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.movies_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
-    id integer NOT NULL,
-    first_name character varying(255),
-    last_name character varying(255),
-    email character varying(255),
-    password character varying(255),
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
-);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
+    id          serial primary key,
+    first_name  character varying(255),
+    last_name   character varying(255),
+    email       character varying(255),
+    password    character varying(255),
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone
 );
 
 
@@ -184,84 +109,3 @@ COPY public.movies_genres (id, movie_id, genre_id) FROM stdin;
 COPY public.users (id, first_name, last_name, email, password, created_at, updated_at) FROM stdin;
 1	Admin	User	admin@example.com	$2a$14$wVsaPvJnJJsomWArouWCtusem6S/.Gauq/GjOIEHpyh2DAMmso1wy	2022-09-23 00:00:00	2022-09-23 00:00:00
 \.
-
-
---
--- Name: genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.genres_id_seq', 13, true);
-
-
---
--- Name: movies_genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.movies_genres_id_seq', 6, true);
-
-
---
--- Name: movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.movies_id_seq', 3, true);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
-
-
---
--- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.genres
-    ADD CONSTRAINT genres_pkey PRIMARY KEY (id);
-
-
---
--- Name: movies_genres movies_genres_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.movies_genres
-    ADD CONSTRAINT movies_genres_pkey PRIMARY KEY (id);
-
-
---
--- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.movies
-    ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: movies_genres movies_genres_genre_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.movies_genres
-    ADD CONSTRAINT movies_genres_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES public.genres(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: movies_genres movies_genres_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.movies_genres
-    ADD CONSTRAINT movies_genres_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- PostgreSQL database dump complete
---
